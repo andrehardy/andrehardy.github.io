53880c7050918019f81d64235c21ef81e5e37dbd
AndreHardy <andre.hardy77@gmail.com>
2021-01-08 15:13:52 -0500

    changing my portfolio

---
+++ script.js
@@ -0,0 +1,22 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
+$(document.body).ready(function() {
+	
+                var hi = $('.roll');
+            
+            
+                TweenMax.from(hi, 5, {
+                    y: 140,
+                    opacity: 0,
+                    ease: Elastic.easeOut
+                })
+            });
